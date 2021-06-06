613ed69fdf5357f7aeebbbfc02bd6845d5d1387d
Vikas Sharma <mailbox.viksharma@gmail.com>
2021-06-06 17:48:07 +0530

    Ui changes

---
+++ ClientApp/src/components/Home.vue
@@ -0,0 +1,47 @@
+<template>
+  <form class="form">
+    <div class="rules__form">
+      <img src="../resources/images/main.png" />
+      <h1>Rules</h1>
+      <label class="rules__text">
+        "Scissors decapitate Scissors cuts paper, paper covers rock, rock
+        crushes lizard, lizard poisons Spock, Spock smashes scissors, scissors
+        decapitates lizard, lizard eats paper, paper disproves Spock, Spock
+        vaporizes rock, and as it always has, rock crushes scissors."
+      </label>
+    </div>
+  </form>
+</template>
+
+<script>
+import Vue from "vue";
+export default Vue.extend({
+  name: "Home",
+});
+</script>
+
+<style>
+.form {
+  width: 406px !important;
+}
+.rules__form {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+.rules__text {
+  margin-right: 8px;
+  box-shadow: inset 2px 2px 5px #babecc, inset -5px -5px 10px #fff;
+  width: 100%;
+  box-sizing: border-box;
+  transition: all 0.2s ease-in-out;
+  appearance: none;
+  -webkit-appearance: none;
+  padding: 2em;
+}
+.rules__text:hover{
+	color: #ae1100;
+  box-shadow: inset 1px 1px 2px #babecc, inset -1px -1px 2px #fff;
+}
+
+</style>
--- ClientApp/src/router.ts
+++ ClientApp/src/router.ts
@@ -2,13 +2,15 @@ import Vue from 'vue';
 import VueRouter from 'vue-router'
 import Profile from './components/Profile.vue';
 import Game from './components/Game.vue';
+import Home from './components/Home.vue';
 
 
 Vue.use(VueRouter);
 
 const routes = [
+	{ path: '/', component: Home, name: 'home' },
 	{ path: '/profile', component: Profile, name: 'profile' },
-	{ path: '/game', component: Game, name: 'game' },	
+	{ path: '/game', component: Game, name: 'game' },
 
 ];
 
