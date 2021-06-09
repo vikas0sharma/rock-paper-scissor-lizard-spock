d3307e9218ec0ced8e3a0e7b155cfa5b1ab86552
Vikas Sharma <mailbox.viksharma@gmail.com>
2021-06-09 22:39:33 +0530

    WIP

--- Models/Game.cs
+++ Models/Game.cs
@@ -28,6 +28,125 @@ namespace RockPaperScissorLizardSpock.Models
             var player = Players.FirstOrDefault(p => p.Id == playerId);
             player.Choice = choice;
         }
+        public string Winner { get; set; }
+        public void GetWinner()
+        {
+            Player[] winners = ChooseWinner(Players);
+        }
+        public Player[] ChooseWinner(Player[] players)
+        {
+            if (players.Any(p => p.Choice == Choice.Unknown)) return new Player[] { };
+            if (players.Length == 1) return players;
+
+            List<Player> newWinners = new List<Player>();
+
+            for (int i = 0; i < players.Length - 1; i++)
+            {
+                for (int j = 0; j < players.Length - i - 1; j++)
+                {
+                    int nxt = j + 1;
+                    if (players[j].Choice == players[nxt].Choice) break;
+
+                    if(players[j].Choice == Choice.Rock)
+                    {
+                        if(players[nxt].Choice == Choice.Lizard)
+                        {
+                            newWinners.Add(players[j]);
+                        }
+                        else if(players[nxt].Choice == Choice.Scissor)
+                        {
+                            newWinners.Add(players[j]);
+                        }
+                        else if (players[nxt].Choice == Choice.Paper)
+                        {
+                            newWinners.Add(players[nxt]);
+                        }
+                        else if (players[nxt].Choice == Choice.Spock)
+                        {
+                            newWinners.Add(players[nxt]);
+                        }
+                    }
+                    else if(players[j].Choice == Choice.Paper)
+                    {
+                        if (players[nxt].Choice == Choice.Lizard)
+                        {
+                            newWinners.Add(players[nxt]);
+                        }
+                        else if (players[nxt].Choice == Choice.Scissor)
+                        {
+                            newWinners.Add(players[nxt]);
+                        }
+                        else if (players[nxt].Choice == Choice.Rock)
+                        {
+                            newWinners.Add(players[j]);
+                        }
+                        else if (players[nxt].Choice == Choice.Spock)
+                        {
+                            newWinners.Add(players[j]);
+                        }
+                    }
+                    else if (players[j].Choice == Choice.Scissor)
+                    {
+                        if (players[nxt].Choice == Choice.Lizard)
+                        {
+                            newWinners.Add(players[j]);
+                        }
+                        else if (players[nxt].Choice == Choice.Paper)
+                        {
+                            newWinners.Add(players[j]);
+                        }
+                        else if (players[nxt].Choice == Choice.Rock)
+                        {
+                            newWinners.Add(players[nxt]);
+                        }
+                        else if (players[nxt].Choice == Choice.Spock)
+                        {
+                            newWinners.Add(players[nxt]);
+                        }
+                    }
+                    else if (players[j].Choice == Choice.Lizard)
+                    {
+                        if (players[nxt].Choice == Choice.Paper)
+                        {
+                            newWinners.Add(players[j]);
+                        }
+                        else if (players[nxt].Choice == Choice.Scissor)
+                        {
+                            newWinners.Add(players[nxt]);
+                        }
+                        else if (players[nxt].Choice == Choice.Rock)
+                        {
+                            newWinners.Add(players[nxt]);
+                        }
+                        else if (players[nxt].Choice == Choice.Spock)
+                        {
+                            newWinners.Add(players[j]);
+                        }
+                    }
+                    else if (players[j].Choice == Choice.Spock)
+                    {
+                        if (players[nxt].Choice == Choice.Paper)
+                        {
+                            newWinners.Add(players[nxt]);
+                        }
+                        else if (players[nxt].Choice == Choice.Scissor)
+                        {
+                            newWinners.Add(players[j]);
+                        }
+                        else if (players[nxt].Choice == Choice.Rock)
+                        {
+                            newWinners.Add(players[j]);
+                        }
+                        else if (players[nxt].Choice == Choice.Lizard)
+                        {
+                            newWinners.Add(players[nxt]);
+                        }
+                    }
+                }
+            }
+
+            return ChooseWinner(newWinners.ToArray());
+        }
 
         public override string ToString() => $"Id: {Id}, Players: {Players}";
     }
