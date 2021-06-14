26dc8144e129d3f652de4b2b059266154861dff5
Vikas Sharma <mailbox.viksharma@gmail.com>
2021-06-14 22:14:39 +0530

    flex and css improved

--- ClientApp/src/components/Game.vue
+++ ClientApp/src/components/Game.vue
@@ -1,47 +1,70 @@
 <template>
-  <div class="container">
-    <h1>Winner: {{winner.name ? winner.name : ''}}</h1>
-    <div class="game">
-      <div class="player">{{ you.name }}</div>
-      <div class="game__options">
-        <div class="option btn btn-primary rounded-bottom" v-on:click="updateChoice(1)">
-          <img src="../resources/images/rock-emoji.png" height="100" />
-        </div>
-        <div class="option btn btn-primary rounded-bottom" v-on:click="updateChoice(2)">
-          <img src="../resources/images/pen-paper-icon-1.png" height="100" />
-        </div>
-        <div class="option btn btn-primary rounded-bottom" v-on:click="updateChoice(3)">
-          <img src="../resources/images/icon-scissors-60.png" height="100" />
-        </div>
-      </div>
-      <div class="game__options">
-        <div class="option btn btn-primary rounded-bottom" v-on:click="updateChoice(4)">
-          <img src="../resources/images/lizard-icon-7.png" height="100" />
-        </div>
-        <div class="option btn btn-primary rounded-bottom" v-on:click="updateChoice(5)">
-          <img src="../resources/images/spock-icon-3.png" height="100" />
-        </div>
-      </div>
-    </div>
-    <div class="game">
-      <div class="player">?</div>
-      <ul>
-        <li v-for="player of opponents" :key="player.id">
-          <div class="card shadow-soft opponent">
-            <div class="">
-              <div class="profile-image shadow-inset rounded mt-n6">
-                <span class="material-icons" v-bind:style="{ color: playerColor }"
-              >face</span
-            >
-              </div>
-            </div>
-            <span class="material-icons" v-bind:style="{ color: playerColor }"
-              >face</span
-            >
-            <span>{{ player.name }}</span>
+  <div>
+    <h1>Winner: {{ winner.name ? winner.name : "" }}</h1>
+    <div class="container">
+      <div class="game">
+        <div class="player">{{ you.name }}</div>
+        <div class="game__options">
+          <div
+            class="option btn btn-primary rounded-bottom"
+            v-on:click="updateChoice(1)"
+          >
+            <img src="../resources/images/rock-emoji.png" height="100" />
           </div>
-        </li>
-      </ul>
+          <div
+            class="option btn btn-primary rounded-bottom"
+            v-on:click="updateChoice(2)"
+          >
+            <img src="../resources/images/pen-paper-icon-1.png" height="100" />
+          </div>
+          <div
+            class="option btn btn-primary rounded-bottom"
+            v-on:click="updateChoice(3)"
+          >
+            <img src="../resources/images/icon-scissors-60.png" height="100" />
+          </div>
+        </div>
+        <div class="game__options">
+          <div
+            class="option btn btn-primary rounded-bottom"
+            v-on:click="updateChoice(4)"
+          >
+            <img src="../resources/images/lizard-icon-7.png" height="100" />
+          </div>
+          <div
+            class="option btn btn-primary rounded-bottom"
+            v-on:click="updateChoice(5)"
+          >
+            <img src="../resources/images/spock-icon-3.png" height="100" />
+          </div>
+        </div>
+      </div>
+      <div class="game">
+        <div class="player">?</div>
+        <ul>
+          <li v-for="player of opponents" :key="player.id">
+            <div class="card shadow-soft opponent">
+              <h2>{{ player.name }}</h2>
+              <hr />
+              <span class="fs-2">Score: 5</span>
+              <!-- <div class="player-avatar shadow-inset rounded">
+              <span
+                class="material-icons fs-3"
+                v-bind:style="{ color: playerColor() }"
+                >face</span
+              >
+              <span class="fs-3">{{ player.name }}</span>
+            </div>
+            <div class="shadow-inset player-avatar rounded mt-10 fs-2">
+              vikas
+            </div>
+            <div class="shadow-inset rounded mt-10 fs-2">
+              vikas
+            </div> -->
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -72,13 +95,17 @@ export default Vue.extend({
     gameId: "",
     playerId: "",
     you: <Player>{},
-    winner: <Player>{}
+    winner: <Player>{},
   }),
   mounted: async function () {
     this.gameId = this.$route.params.game_id;
     this.playerId = this.$route.params.player_id;
 
-    await setUpSignalRConnection(this.gameId, this.onplayersUpdated, this.onWinnerChanged);
+    await setUpSignalRConnection(
+      this.gameId,
+      this.onplayersUpdated,
+      this.onWinnerChanged
+    );
     this.onplayersUpdated(await getPlayers(this.gameId));
     this.you = this.players.find((p) => p.id === this.playerId) as Player;
   },
@@ -86,23 +113,23 @@ export default Vue.extend({
     onplayersUpdated: function (players: Player[]) {
       this.players = players;
     },
-    onWinnerChanged : function(winner: Player){
+    onWinnerChanged: function (winner: Player) {
       this.winner = winner;
     },
     updateChoice: async function (choice: Choice) {
       this.you.choice = choice;
       await updatePlayerChoice(this.gameId, this.you);
     },
+    playerColor: function () {
+      const rnd = Math.floor(Math.random() * this.colors.length);
+      console.log(this.colors);
+      return this.colors[rnd];
+    },
   },
   computed: {
     opponents: function () {
       const playerId = this.$route.params.player_id;
       return this.players.filter((p) => p.id !== playerId);
-    },
-    playerColor: function () {
-      const rnd = Math.floor(Math.random() * this.colors.length);
-      console.log(this.colors);
-      return this.colors[rnd];
     },
   },
 });
@@ -114,14 +141,16 @@ ul {
 }
 .container {
   display: flex;
-  justify-content: space-between;
+  justify-content: space-around;
+  justify-self:center;
+  flex-flow: wrap;
+  align-content: center;
 }
 .game {
   display: flex;
   flex-direction: column;
   align-items: center;
   margin-top: 50px;
-  width: 50%;
 }
 .game__options {
   display: flex;
@@ -145,5 +174,20 @@ ul {
   display: flex;
   justify-content: center;
   flex-wrap: wrap;
+}
+.player-avatar {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.fs-3 {
+  font-size: 3em;
+}
+.fs-2 {
+  font-size: 1.5em;
+}
+.mt-10 {
+  margin-top: 10px;
 }
 </style>
